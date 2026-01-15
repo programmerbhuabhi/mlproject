5be2b618daf220e9244bcd018eafb4e202bc7212
Abhishek <abhishekanandmahto@gmail.com>
2026-01-15 22:18:18 +0530

    model is completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,12 +2,14 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_training import ModelTrainerConfig
+from src.components.model_training import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -45,7 +47,11 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     
             
     
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -72,6 +72,7 @@ class DataTransformation:
             input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
             logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
+
             input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessor_obj.transform(input_feature_test_df)
             train_arr = np.c_[
